4e45871edf991df09699bf5e41dc7a947adcc67b
Brad Turner <dev.bradturner@gmail.com>
2020-03-02 10:00:20 +1100

    added template copies

--- cli.js
+++ cli.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const { exec } = require('child_process');
 const inquirer = require('inquirer');
 
 const [, , ...args] = process.argv;
 
-const isTest = args[0] || false;
+const isTest = args[0] === '--test' || false;
 
 async function main(isTest = false) {
   const prompt = inquirer.createPromptModule();
 
+  const commands = [`npm init -y ${isTest ? '--dry-run' : ''}`];
   const packages = [];
-  const devPackages = ['eslint'];
+  const devPackages = ['eslint', 'cross-env'];
+
+  if (isTest) console.log('Running as a test...');
 
   const { isTypescript } = await prompt([
     { type: 'confirm', name: 'isTypescript', message: 'Install Typescript?', default: true }
@@ -34,6 +39,7 @@ async function main(isTest = false) {
     devPackages.push(
       'webpack',
       'webpack-cli',
+      'webpack-dev-server',
       '@babel/core',
       '@babel/preset-env',
       '@babel/preset-react',
@@ -66,13 +72,22 @@ async function main(isTest = false) {
     }
   }
 
-  const commands = [`npm init -y ${isTest ? '--dry-run' : ''}`];
+  if (!isTest) {
+    //const gitignore = `echo "node_modules/${isReact ? '\ndist/' : ''}" >> .gitignore`;
+    //console.log('gitignore', gitignore);
+    commands.push('mkdir src');
+
+    if (isReact) commands.push('mkdir src/components');
+  }
+
   if (packages.length > 0) commands.push([`npm install ${packages.join(' ')} ${isTest ? '--dry-run' : ''}`]);
   if (devPackages.length > 0) {
     commands.push([`npm install --save-dev ${devPackages.join(' ')} ${isTest ? '--dry-run' : ''}`]);
   }
 
-  exec(commands.join(' & '), (err, stdout, stderr) => {
+  console.log(commands.join(' && '));
+
+  exec(commands.join(' && '), (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
@@ -83,6 +98,59 @@ async function main(isTest = false) {
     }
     console.log(stdout);
   });
+
+  console.log(
+    `Copying >> ${path.resolve(__dirname, 'templates', 'tsconfig.template.json')}\nTo >> ${path.resolve(
+      process.cwd(),
+      'tsconfig.json'
+    )}`
+  );
+
+  if (!isTest) {
+    // Copy TS config.
+    if (isTypescript) {
+      fs.copyFile(
+        path.resolve(__dirname, 'templates', 'tsconfig.template.json'),
+        path.resolve(process.cwd(), 'tsconfig.json'),
+        err => {
+          if (err) throw err;
+          console.log('Successfully created Typescript config.');
+        }
+      );
+    }
+
+    // Copy WEBPACK config.
+    if (isReact) {
+      if (isTypescript) {
+        fs.copyFile(
+          path.resolve(__dirname, 'templates', 'tsx-webpack.template.js'),
+          path.resolve(process.cwd(), 'webpack.config.js'),
+          err => {
+            if (err) throw err;
+            console.log('Successfully created Webpack config.');
+          }
+        );
+      } else {
+        fs.copyFile(
+          path.resolve(__dirname, 'templates', 'jsx-webpack.template.js'),
+          path.resolve(process.cwd(), 'webpack.config.js'),
+          err => {
+            if (err) throw err;
+            console.log('Successfully created Webpack config.');
+          }
+        );
+      }
+
+      fs.copyFile(
+        path.resolve(__dirname, 'templates', 'tempate.html'),
+        path.resolve(process.cwd(), 'templates', 'index,html'),
+        err => {
+          if (err) throw err;
+          console.log('Successfully created index.html template.');
+        }
+      );
+    }
+  }
 }
 
 main(isTest);
---
+++ templates/jsx-webpack.template.js
@@ -0,0 +1,86 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = ({ NODE_ENV }) => {
+  const config = {
+    devtool: NODE_ENV === 'production' ? undefined : 'source-map',
+    entry: ['./src/index.tsx'],
+    mode: NODE_ENV,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: { loader: 'babel-loader' }
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            'style-loader', // Creates style nodes from JS strings.
+            'css-loader', // Translates CSS into CommonJS.
+            'sass-loader' // Compiles Sass to CSS.
+          ]
+        },
+        {
+          test: /\.(png|woff|eot|ttf|woff2|svg)(\?.*$|$)/,
+          loader: 'url-loader?limit=100000&mimetype=application/font-woff'
+        },
+        {
+          test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader?name=images/[hash].[ext]'
+        }
+      ]
+    },
+
+    optimization: {
+      runtimeChunk: false
+    },
+
+    output: {
+      filename: 'bundle.js',
+      library: 'AppBundle',
+      libraryTarget: 'window',
+      path: path.resolve(__dirname, 'dist'),
+      pathinfo: NODE_ENV === 'production' ? false : true
+    },
+
+    plugins: [
+      new CleanWebpackPlugin() // DEFAULTS TO READING THE DIST FILE.
+    ],
+
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    }
+  };
+
+  if (NODE_ENV === 'development') {
+    config.entry.unshift('./src/app-init.js');
+
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        title: 'Dev App',
+        template: './templates/index.html'
+      })
+    );
+
+    config.devServer = {
+      port: 3000,
+      open: true
+    };
+  }
+
+  if (NODE_ENV === 'production') {
+    config.optimization.minimize = true;
+  }
+
+  return config;
+};
+
+module.exports = [config(process.env)];
